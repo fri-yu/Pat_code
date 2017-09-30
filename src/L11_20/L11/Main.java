43426bd5d4003022ce23fc711e97a272e654b6b0
jieliyu <jieliyu@houbank.cn>
2017-09-30 18:12:42 +0800

    L11

--- src/L11_20/L11/Main.java
+++ src/L11_20/L11/Main.java
@@ -3,8 +3,19 @@ package L11_20.L11;
 /**
  * Package : L11_20.L11
  *
- * @author YixinCapital -- jieliyu
- *         2017年09月30日 17:13:00
+ * @author YixinCapital -- jieliyu 2017年09月30日 17:13:00
  */
+import java.util.Scanner;
+
 public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int lineNumber = sc.nextInt();
+		for (int i = 0; i < lineNumber; i++) {
+			long a = sc.nextLong();
+			long b = sc.nextLong();
+			long c = sc.nextLong();
+			System.out.println("Case #" + (i + 1) + ": " + (a - c + b > 0));
+		}
+	}
 }
